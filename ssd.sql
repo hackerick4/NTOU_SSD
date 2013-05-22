f968a7b55b2ab4500ad3dd623098041775ebe1b0
Rick <hackerick4@gmail.com>
2013-05-23 00:42:23 +0800

    更新資料庫schema

--- ssd.sql
+++ ssd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2013 at 06:32 PM
+-- Generation Time: May 22, 2013 at 06:41 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -32,7 +32,8 @@ CREATE TABLE IF NOT EXISTS `course_info` (
   `course_name` varchar(255) NOT NULL,
   `course_time` varchar(255) NOT NULL,
   `teacher` varchar(255) NOT NULL,
-  `rating` int(11) unsigned NOT NULL DEFAULT '0',
+  `rating` int(11) unsigned NOT NULL DEFAULT '2',
+  `rateCount` int(10) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`course_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Record course information ';
 
@@ -40,9 +41,9 @@ CREATE TABLE IF NOT EXISTS `course_info` (
 -- Dumping data for table `course_info`
 --
 
-INSERT INTO `course_info` (`department`, `course_ID`, `course_name`, `course_time`, `teacher`, `rating`) VALUES
-('資工系', 'B1234567', '軟體工程', '202,203,204', '馬上冰', 2),
-('商船系', 'B7654321', '微積分', '102,203,204', '程懷懷', 4);
+INSERT INTO `course_info` (`department`, `course_ID`, `course_name`, `course_time`, `teacher`, `rating`, `rateCount`) VALUES
+('資工系', 'B1234567', '軟體工程', '202,203,204', '馬上冰', 1, 5),
+('商船系', 'B7654321', '微積分', '102,203,204', '程懷懷', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -58,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `current_posts` (
   `recieve_course_ID` varchar(20) DEFAULT NULL,
   `PostID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`PostID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='the information at the mainPage' AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='the information at the mainPage' AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `current_posts`
@@ -66,7 +67,8 @@ CREATE TABLE IF NOT EXISTS `current_posts` (
 
 INSERT INTO `current_posts` (`fb_ID`, `post_time`, `send_course_ID`, `state`, `recieve_course_ID`, `PostID`) VALUES
 ('123456789123456', '2013-05-12 18:12:04', 'B1234567', 'ready', 'B7654321', 1),
-('123654987456214', '2013-05-14 16:29:41', 'M897542', 'ready', 'B8547896', 4);
+('123654987456214', '2013-05-15 15:55:18', 'M897542', 'ready', 'NULL', 16),
+('123456789569874', '2013-05-21 18:52:25', 'M897542', 'ready', 'NULL', 17);
 
 -- --------------------------------------------------------
 
@@ -77,8 +79,8 @@ INSERT INTO `current_posts` (`fb_ID`, `post_time`, `send_course_ID`, `state`, `r
 CREATE TABLE IF NOT EXISTS `user` (
   `user_name` varchar(20) NOT NULL,
   `fb_ID` varchar(20) NOT NULL,
-  `login_token` varchar(50) NOT NULL,
   `right_point` int(11) NOT NULL DEFAULT '1',
+  `ratedCourses` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`fb_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Record users'' information';
 
@@ -86,8 +88,10 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_name`, `fb_ID`, `login_token`, `right_point`) VALUES
-('詹肥肥', '123456789123456', 'LoginSuccess12345', 1);
+INSERT INTO `user` (`user_name`, `fb_ID`, `right_point`, `ratedCourses`) VALUES
+('詹肥肥', '123456789123456', 2, 'B1234567,B7654321'),
+('詹肥肥3號', '123456789981234', 3, NULL),
+('詹肥肥2號', '123456789987654', 1, 'B1234567,B1234888');
 
 -- --------------------------------------------------------
 
